9ae9b2495d52c556bfb7b2dd6e883c98f6e279db
meririm <cindy@mostlypixels.com>
2014-07-26 20:44:48 +0200

    1000 odds class

---
+++ Odds/Thousand.php
@@ -0,0 +1,12 @@
+<?php
+namespace Futilities;
+
+class Odds_Thousand extends Odds
+{
+    const COMMON = 600;
+    const UNCOMMON = 250;
+    const RARE = 100;
+    const LEGENDARY = 40;
+    const EPIC = 10;
+    const MIRACULOUS = 0;
+}
